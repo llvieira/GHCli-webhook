0160dbb53127e094c929ce8ac9b387cef1a7cd0b
Lucas Vieira <lima.lucaslv@gmail.com>
2019-09-03 15:03:23 -0300

    Add get notifications function

--- index.js
+++ index.js
@@ -1,24 +1,43 @@
 var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
+const request = require('request');
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
+var users = {}
+
 io.on('connection', function(socket){
   socket.on('message', function (data) {
-    console.log(data.email);
-    socket.join(data.email); // We are using room of socket io
-
-    // get notifications from github for this user and send it
-    io.in(data.email).emit('new_msg', {msg: 'hello'})
+  	users[data.email] = {data: data, socket: socket}
   });
 });
+
+setInterval(function(){ getNotifications() }, 5000);
+
+function getNotifications() {
+	Object.keys(users).forEach(function(key) {
+  		var gitCredentials = users[key].data.credentials;
+  		var notificationsDate = "";
+  		var userSocket = users[key].socket;
+
+  		// TODO
+  		request('https://api.github.com/notifications?since=2014-11-07T08:00:00Z', { json: true }, (err, res, body) => {
+  			if (err) { 
+  				return console.log(err); 
+  			}
+
+  			userSocket.emit('new_msg', body);
+		});
+	});
+}
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
 
-// https://socket.io/get-started/chat
+// https://socket.io/get-started/chat]
+// https://developer.github.com/v3/activity/notifications/
 // https://stackoverflow.com/questions/17476294/how-to-send-a-message-to-a-particular-client-with-socket-io
